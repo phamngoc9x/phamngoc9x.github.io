68acc468235451691fd5479c11a849a113d56ca9
pham.van.ngoc <pham.van.ngoc@B121645-mb.local>
2020-08-28 09:53:32 +0700

    updaste

--- barcode/dest/js/main.js
+++ barcode/dest/js/main.js
@@ -131,11 +131,11 @@ const ChutProject = {
     // Create the QuaggaJS config object for the live stream
     var liveStreamConfig = {
       inputStream: {
-        type : "LiveStream",
+        type: "LiveStream",
         constraints: {
-          width: {min: 320},
-          height: {min: 240},
-          aspectRatio: {min: 1, max: 100},
+          width: { min: 640 },
+          height: { min: 480 },
+          aspectRatio: { min: 1, max: 100 },
           facingMode: "environment" // or "user" for the front camera
         }
       },
@@ -143,41 +143,38 @@ const ChutProject = {
         patchSize: "medium",
         halfSample: true
       },
-      numOfWorkers: (navigator.hardwareConcurrency ? navigator.hardwareConcurrency : 4),
+      numOfWorkers: navigator.hardwareConcurrency
+        ? navigator.hardwareConcurrency
+        : 4,
       decoder: {
-        "readers":[
-          {"format":"ean_reader","config":{}},
-          {"format":"code_39_reader","config":{}},
-          {"format":"code_39_vin_reader","config":{}}
-        ]
+        readers: [{ format: "ean_reader", config: {} }]
       },
       locate: true
     };
     // The fallback to the file API requires a different inputStream option.
     // The rest is the same
-    var fileConfig = $.extend(
-        {},
-        liveStreamConfig,
-        {
-          inputStream: {
-            size: 320
-          }
-        }
-      );
+    var fileConfig = $.extend({}, liveStreamConfig, {
+      inputStream: {
+        size: 800
+      }
+    });
     // Start the live stream scanner when the modal opens
-    $('.modal-barcode').on('shown.bs.modal', function (e) {
-      Quagga.init(
-        liveStreamConfig,
-        function(err) {
-          if (err) {
-            $('.modal-barcode .modal-body .error').html('<div class="alert alert-danger"><strong><i class="fa fa-exclamation-triangle"></i> '+err.name+'</strong>: '+err.message+'</div>');
-            Quagga.stop();
-            return;
-          }
-          Quagga.start();
+    $(".modal-barcode").on("shown.bs.modal", function(e) {
+      Quagga.init(liveStreamConfig, function(err) {
+        if (err) {
+          $(".modal-barcode .modal-body .error").html(
+            '<div class="alert alert-danger"><strong><i class="fa fa-exclamation-triangle"></i> ' +
+              err.name +
+              "</strong>: " +
+              err.message +
+              "</div>"
+          );
+          Quagga.stop();
+          return;
         }
-      );
+        Quagga.start();
       });
+    });
 
     // Make sure, QuaggaJS draws frames an lines around possible
     // barcodes on the live stream
@@ -187,20 +184,38 @@ const ChutProject = {
 
       if (result) {
         if (result.boxes) {
-          drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-          result.boxes.filter(function (box) {
-            return box !== result.box;
-          }).forEach(function (box) {
-            Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-          });
+          drawingCtx.clearRect(
+            0,
+            0,
+            parseInt(drawingCanvas.getAttribute("width")),
+            parseInt(drawingCanvas.getAttribute("height"))
+          );
+          result.boxes
+            .filter(function(box) {
+              return box !== result.box;
+            })
+            .forEach(function(box) {
+              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+                color: "green",
+                lineWidth: 2
+              });
+            });
         }
 
         if (result.box) {
-          Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+            color: "#00F",
+            lineWidth: 2
+          });
         }
 
         if (result.codeResult && result.codeResult.code) {
-          Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+          Quagga.ImageDebug.drawPath(
+            result.line,
+            { x: "x", y: "y" },
+            drawingCtx,
+            { color: "red", lineWidth: 3 }
+          );
         }
       }
     });
@@ -209,24 +224,36 @@ const ChutProject = {
     // close the modal after a second to let the user notice where
     // the barcode had actually been found.
     Quagga.onDetected(function(result) {
-      if (result.codeResult.code){
-        $('#scanner_input').val(result.codeResult.code);
-        Quagga.stop();
+      if (result.codeResult.code) {
+        
+        $("#scanner_input").val(result.codeResult.code);
+        
+        // Quagga.stop();
+        // setTimeout(function() {
+        //   $(".modal-barcode").modal("hide");
+        // }, 1000);
       }
     });
 
     // Stop quagga in any case, when the modal is closed
-      $('.modal-barcode').on('hide.bs.modal', function(){
-        if (Quagga){
-          Quagga.stop();
-        }
-      });
+    $(".modal-barcode").on("hide.bs.modal", function() {
+      if (Quagga) {
+        Quagga.stop();
+      }
+    });
 
     // Call Quagga.decodeSingle() for every file selected in the
     // file input
     $(".modal-barcode input:file").on("change", function(e) {
       if (e.target.files && e.target.files.length) {
-        Quagga.decodeSingle($.extend({}, fileConfig, {src: URL.createObjectURL(e.target.files[0])}), function(result) {alert(result.codeResult.code);});
+        Quagga.decodeSingle(
+          $.extend({}, fileConfig, {
+            src: URL.createObjectURL(e.target.files[0])
+          }),
+          function(result) {
+            alert(result.codeResult.code);
+          }
+        );
       }
     });
   },
@@ -239,4 +266,3 @@ $(document).ready(function() {
 $(window).on('resize', function() {
   ChutProject.Init();
 });
-
